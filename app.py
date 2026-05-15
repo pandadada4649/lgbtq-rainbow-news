d083c0a0833285c979e62dbdd721c5bffa0bed03
pandadada4649 <pandadada4649@yahoo.co.jp>
2026-05-15 16:56:40 +0900

    fix: DB migration

--- app.py
+++ app.py
@@ -306,4 +306,6 @@ def api_stats():
     return jsonify(data)
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5001)), debug=True)
